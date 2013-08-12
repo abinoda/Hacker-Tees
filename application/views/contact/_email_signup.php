b4fea9d5d2f2c74bd7bdaf37f1817e5408694ff0
Abi Noda <a@abinoda.com>
2013-08-11 21:12:14 -0500

    form field placeholder

--- application/views/contact/_email_signup.php
+++ application/views/contact/_email_signup.php
@@ -3,7 +3,7 @@
     <?= Form::open(URL::site('email_signup')) ?>
         <?= CSRF::set_token() ?>
         <div>
-            <?= Form::input('email', 'enter your email', array('id' => 'email')) ?>
+            <?= Form::input('email', 'Your email address', array('id' => 'email', 'onfocus' => "this.value=''", 'onblur' => "if(this.value=='') { this.value='Your email address'}")) ?>
             <?= Form::submit(NULL, 'submit', array('class' => 'submit')) ?>
         </div>
     <?= Form::close() ?>
